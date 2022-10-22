2287d2d60f133c2ca6973c3690b3936dc3be7bca
le mak <yvesmak1@gmail.com>
2022-10-22 09:35:51 +0200

    les condition

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice Php</title>
+</head>
+<body>
+    
+</body>
+ <?php 
+    $valeur = 10 ;
+    switch($valeur){
+        case 0:
+            echo "Tu es nul";
+        break;
+
+        case 10:
+            echo "Tu as réussi le test";
+        break;
+
+        default:
+            echo "fin de l'instruction";
+            
+    }
+ ?>
+</html>

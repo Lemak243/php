e51a24f365383358470df2fc1e67fac092b7b1f3
le mak <yvesmak1@gmail.com>
2022-12-08 09:27:20 +0100

    les includes

---
+++ blocsFonctionnels/functions.php
@@ -0,0 +1,36 @@
+<?php
+// functions.php
+
+function isValidRecipe(array $recipe)
+{
+    if (array_key_exists('is_enabled', $recipe)) {
+        $isEnabled = $recipe['is_enabled'];
+    } else {
+        $isEnabled = false;
+    }
+
+    return $isEnabled;
+}
+
+function displayAuthor(string $authorEmail, array $users)
+{
+    for ($i = 0; $i < count($users); $i++) {
+        $author = $users[$i];
+        if ($authorEmail === $author['email']) {
+            return $author['full_name'] . '(' . $author['age'] . ' ans)';
+        }
+    }
+}
+
+function getRecipes(array $recipes)
+{
+    $validRecipes = [];
+
+    foreach($recipes as $recipe) {
+        if (isValidRecipe($recipe)) {
+            $validRecipes[] = $recipe;
+        }
+    }
+
+    return $validRecipes;
+}
--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <li><a href="tableau.php">Aller vers mon tableau</a> </li>
     <li><a href="function.php">Aller vers la fonction</a> </li>
     <li><a href="MonScriptErreur.php">Au secours ! Mon script plante !</a> </li>
+    <li><a href="blocsFonctionnels/index.php">Organiser les pages du site web en blocs fonctionnels</a> </li>
     <li><a href="api.php">API</a> </li>
   </ul>
 </body>
